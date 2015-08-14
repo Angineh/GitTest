ee97c3ba289c974e32dfc5333b6ce179aa0f5473
Ipshita <iroyburman89@gmail.com>
2015-08-14 16:15:59 -0700

    Another Class added

---
+++ HelloWorld/src/AnotherClass.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ipshita
+ *
+ */
+public class AnotherClass {
+
+	/**
+	 * 
+	 */
+	public AnotherClass() {
+System.out.println("another print statement ha!");	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
