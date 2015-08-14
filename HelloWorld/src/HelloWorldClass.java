fcd43bda4ba201e5e1b159a2ba7cb1625b74e746
Ipshita <iroyburman89@gmail.com>
2015-08-14 16:24:45 -0700

    Fixed the first print statement

--- HelloWorld/src/HelloWorldClass.java
+++ HelloWorld/src/HelloWorldClass.java
@@ -2,7 +2,7 @@
 public class HelloWorldClass {
 
 	public static void main(String[] args) {
-		System.out.println("finally added a print statement!");
+		System.out.println("fixed the print statement!");
 
 	}
 
